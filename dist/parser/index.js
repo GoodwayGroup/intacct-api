a7b204b2c7fabe193a7b3e24c4b0ad0a4c180439
Nate Gardner <ngardner@goodwaygroup.com>
2020-05-28 12:56:04 -0400

    testing dist dir

---
+++ dist/parser/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _inspect = require('./inspect');
+
+var _error_message = require('./error_message');
+
+var _read = require('./read');
+
+exports.default = {
+    inspect: _inspect.inspect,
+    errormessage: _error_message.errormessage,
+    read: _read.read,
+    readByQuery: _read.read,
+    readByName: _read.read,
+    readMore: _read.read
+};
+module.exports = exports['default'];
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9wYXJzZXIvaW5kZXguanMiXSwibmFtZXMiOlsiaW5zcGVjdCIsImVycm9ybWVzc2FnZSIsInJlYWQiLCJyZWFkQnlRdWVyeSIsInJlYWRCeU5hbWUiLCJyZWFkTW9yZSJdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUE7O0FBQ0E7O0FBQ0E7O2tCQUVlO0FBQ1hBLDZCQURXO0FBRVhDLDZDQUZXO0FBR1hDLG9CQUhXO0FBSVhDLDJCQUpXO0FBS1hDLDBCQUxXO0FBTVhDO0FBTlcsQyIsImZpbGUiOiJpbmRleC5qcyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IGluc3BlY3QgfSBmcm9tICcuL2luc3BlY3QnO1xuaW1wb3J0IHsgZXJyb3JtZXNzYWdlIH0gZnJvbSAnLi9lcnJvcl9tZXNzYWdlJztcbmltcG9ydCB7IHJlYWQgfSBmcm9tICcuL3JlYWQnO1xuXG5leHBvcnQgZGVmYXVsdCB7XG4gICAgaW5zcGVjdCxcbiAgICBlcnJvcm1lc3NhZ2UsXG4gICAgcmVhZCxcbiAgICByZWFkQnlRdWVyeTogcmVhZCxcbiAgICByZWFkQnlOYW1lOiByZWFkLFxuICAgIHJlYWRNb3JlOiByZWFkXG59O1xuIl19
